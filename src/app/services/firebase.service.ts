aef65f8e766330ed7d6070accec9d69d2fba5cf4
JuanDavid004 <juandavid.prieto0694@gmail.com>
2025-02-24 13:53:54 +0100

    pasos del 9 al 20

---
+++ src/app/services/firebase.service.ts
@@ -0,0 +1,93 @@
+import { Injectable, inject } from '@angular/core';
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+  UserCredential,
+  sendPasswordResetEmail,
+} from '@angular/fire/auth';
+import {
+  Storage,
+  uploadString,
+  ref,
+  getDownloadURL,
+} from '@angular/fire/storage';
+import { User } from '../models/user.model';
+import {
+  Firestore,
+  setDoc,
+  doc,
+  getDoc,
+  addDoc,
+  collection,
+} from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FirebaseService {
+  auth = inject(Auth);
+  firestore = inject(Firestore);
+  storage = inject(Storage);
+
+  signIn(user: User): Promise<UserCredential> {
+    return signInWithEmailAndPassword(this.auth, user.email, user.password);
+  }
+
+  signUp(user: User): Promise<UserCredential> {
+    return createUserWithEmailAndPassword(this.auth, user.email, user.password);
+  }
+
+  async updateUser(displayName: string) {
+    const user = await this.auth.currentUser;
+    if (user) {
+      // Actualiza el perfil del usuario
+      await updateProfile(user, { displayName: displayName });
+    }
+  }
+
+  sendRecoveryEmail(email: string) {
+    return sendPasswordResetEmail(this.auth, email);
+  }
+
+  async signOut() {
+    await this.auth.signOut();
+    localStorage.removeItem('user');
+    window.location.reload();
+  }
+
+  async isAuthenticated() {
+    const userExists: boolean = await new Promise((resolve) => {
+      const unsubscribe = this.auth.onAuthStateChanged((user) => {
+        unsubscribe();
+        if (user) {
+          resolve(true);
+        } else {
+          resolve(false);
+        }
+      });
+    });
+    return userExists;
+  }
+
+  async getDocument(path: string) {
+    const docSnap = await getDoc(doc(this.firestore, path));
+    return docSnap.data();
+  }
+
+  setDocument(path: string, data: any) {
+    return setDoc(doc(this.firestore, path), data);
+  }
+  addDocument(path: string, data: any) {
+    return addDoc(collection(this.firestore, path), data);
+  }
+
+  async uploadImage(path: string, imageUrl: string) {
+    return uploadString(ref(this.storage, path), imageUrl, 'data_url').then(
+      () => {
+        return getDownloadURL(ref(this.storage, path));
+      }
+    );
+  }
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBJ2UvcChiXxSnb75WcxK5SDlCaEpTllFs",
+    authDomain: "botellines-siempre.firebaseapp.com",
+    projectId: "botellines-siempre",
+    storageBucket: "botellines-siempre.firebasestorage.app",
+    messagingSenderId: "457954056870",
+    appId: "1:457954056870:web:9e7c1b5758ad3c5a873267"
+  },
 };
